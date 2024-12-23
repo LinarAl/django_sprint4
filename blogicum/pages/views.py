565c51f7b0ac3bb0285eb1f4088913b420ecd04f
Linar Aleev <aleevlr@yandex.ru>
2024-12-24 03:58:27 +0400

    make CBV in pages views for static pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,25 +1,18 @@
-"""Обработка запросов
-Импорт функции render. Две функции обрабатывают запросы: about - страница с
-информацией о проекте, rules - страница правилами для пользователей.
+"""Обработка запросов статичных страниц about и rules
+about - страница с информацией о проекте, rules - страница правилами для
+пользователей.
 """
-from django.shortcuts import render
 
-# Create your views here.
-
-
-def about(request):
-    """Обработка страницы с информацией о проекте.
-    template - шаблон страницы html; Функция возвращает
-    вызов функции render с параметрами(request, template).
-    """
-    template = 'pages/about.html'
-    return render(request, template)
+from django.views.generic import TemplateView
 
 
-def rules(request):
-    """Обработка страницы с правилами для пользователей.
-    template - шаблон страницы html; Функция возвращает
-    вызов функции render с параметрами(request, template).
-    """
-    template = 'pages/rules.html'
-    return render(request, template)
+class AboutPage(TemplateView):
+    """Обработка страницы с информацией о проекте."""
+
+    template_name = 'pages/about.html'
+
+
+class RulesPage(TemplateView):
+    """Обработка страницы с правилами для пользователей."""
+
+    template_name = 'pages/rules.html'
